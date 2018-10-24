3d2ab7d626e7dc3558cc44498b89c0f5cb133a54
Tyler Wray <wraytw@gmail.com>
2018-10-23 20:39:38 -0600

    Adding bucket registry

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -19,13 +19,13 @@ defmodule KV.Bucket do
   Puts a value in the bucket for the key
   """
   def put(bucket, key, value) do
-    Agent.update(bucket, &Map.put(&1, key, value))    
+    Agent.update(bucket, &Map.put(&1, key, value))
   end
 
   @doc """
   Deletes a value from the bucket
   """
   def delete(bucket, key) do
-    Agent.get_and_update(bucket, &Map.pop(&1, key))    
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
   end
 end
